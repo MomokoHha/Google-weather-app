081c28c5f2884aad8c5ac5f4e294102da445fffb
MomokoHha <htethtet007.hha@gmail.com>
2023-05-03 12:20:39 +0630

    updating date

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+// creating new function for date response.data.dt/timestamp=millisec since 1970
+// formatDate function will receive timestamp which is response.data.dt
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Moday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response);
   let cityElement = document.querySelector("#city");
@@ -14,6 +42,9 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = response.data.wind.speed;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "a517e2f077fee2ef9b2aa7d6e87f83b4";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20york&appid=${apiKey}&units=imperial`;
